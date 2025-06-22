080dcd51a23deda17b6777c903a1834ef1ad1289
BahetiGiriraj <bahetigiriraj31@gmail.com>
2025-06-22 19:34:06 +0530

    Done

--- frontend/src/landing_page/login/Login.js
+++ frontend/src/landing_page/login/Login.js
@@ -1,44 +1,108 @@
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 function Login() {
-    return ( 
-        <div className="container mt-5 p-5 ">
-            <div className="row ">
-                <div className="col-4">
-                    <img src="media/images/signup.png"/>
-                </div>
-                <div className="col-2 m-5"></div>
-                <div className="col-5 p-5 ">
-                  <h1>Login</h1>
-                  <form className="p-3" >
-                     <div className="mb-3">
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = inputValue;
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleError = (err) =>
+    toast.error(err, {
+      position: "bottom-left",
+    });
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "bottom-left",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:4000/login",
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+      console.log(data);
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({
+      ...inputValue,
+      email: "",
+      password: "",
+    });
+  };
+  return (
+    <div className="container mt-5 p-5 ">
+      <div className="row ">
+        <div className="col-4">
+          <img src="media/images/signup.png" />
+        </div>
+        <div className="col-2 m-5"></div>
+        <div className="col-5 p-5 ">
+          <h1>Login</h1>
+          <form className="p-3">
+            <div className="mb-3">
+              <label htmlFor="email">Email</label>
               <input
                 type="email"
                 className="form-control"
                 name="email"
                 placeholder="Email"
+                value={email}
                 required
+                onChange={handleOnChange}
               />
             </div>
             <div className="mb-3">
+              <label htmlFor="password">Password</label>
               <input
                 type="password"
                 className="form-control"
                 name="password"
                 placeholder="Password"
+                value={password}
+                onChange={handleOnChange}
                 required
               />
             </div>
             <button type="submit" className="btn btn-primary w-100">
               Login
+             
             </button>
-                  </form>
-                    
-                </div>
-            </div>
+             <span>
+                Already have an account? <Link to={"/signup"}>Signup</Link>
+              </span>
+          </form>
+          <ToastContainer />
         </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Login;
-
--- frontend/src/landing_page/signup/Signup.js
+++ frontend/src/landing_page/signup/Signup.js
@@ -1,62 +1,135 @@
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Signup() {
-    return ( 
-        <div className="container mt-5 p-5 ">
-            <div className="row ">
-                <div className="col-4">
-                    <img src="media/images/signup.png"/>
-                </div>
-                <div className="col-2 m-5"></div>
-                <div className="col-5 p-5 ">
-                  <h1>Signup now</h1>
-                  <p className="text-muted fs-5">Or track your existing application</p>
-                  <form className="p-3" >
-                    <div className="mb-3 input-group ">
-                         <input
-                  type="text"
-                  className="form-control "
-                  name="name"
-                  placeholder="Full Name"
-                  
-                //   value={formData.name}
-                //   onChange={handleChange}
-                  required
-                />
-                    </div>
-                     <div className="mb-3">
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const { email, password, username } = inputValue;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleError = (err) =>
+    toast.error(err, {
+      position: "bottom-left",
+    });
+
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "bottom-right",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3002/signup",
+        { ...inputValue },
+        { withCredentials: true }
+      );
+      const { success, message, user } = data;
+      const {_id} = user
+
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          // navigate(`http://localhost:3001/?id=${_id}`);
+          window.location.href = `http://localhost:3000/?id=${_id}`;
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
+    // Reset form
+    setInputValue({
+      email: "",
+      password: "",
+      username: "",
+    });
+  };
+
+  return (
+    <div className="container mt-5 p-5">
+      <div className="row">
+        <div className="col-4 mt-5">
+          <img src="media/images/signup.png" alt="Signup Illustration" className="img-fluid" />
+        </div>
+
+        <div className="col-2 m-5"></div>
+
+        <div className="col-5 p-5">
+          <h1>Signup now</h1>
+          <p className="text-muted fs-5">Or track your existing application</p>
+
+          <form onSubmit={handleSubmit} className="p-2">
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Full Name"
+                value={username}
+                onChange={handleOnChange}
+                required
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email</label>
               <input
                 type="email"
                 className="form-control"
                 name="email"
                 placeholder="Email"
-                // value={formData.email}
-                // onChange={handleChange}
+                value={email}
+                onChange={handleOnChange}
                 required
               />
             </div>
+
             <div className="mb-3">
+              <label htmlFor="password" className="form-label">Password</label>
               <input
                 type="password"
                 className="form-control"
                 name="password"
                 placeholder="Password"
-                // value={formData.password}
-                // onChange={handleChange}
+                value={password}
+                onChange={handleOnChange}
                 required
               />
             </div>
-            <button type="submit" className="btn btn-primary w-100">
-              {/* {isSignup ? "Sign Up" : "Login"} */}
-              Signup
+
+            <button type="submit" className="btn btn-primary w-100 mb-3">
+              Sign Up
             </button>
-                  </form>
-                    
-                </div>
-            </div>
+
+            <p className="text-center">
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </form>
+
         </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
-
